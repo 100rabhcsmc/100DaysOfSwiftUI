7dad2f5b7cfa903881950250e6b6bdc2b620f9a9
100rabhcsmc <saurabhchavan052@gmail.com>
2022-09-16 21:53:29 +0530

    Day16 added

---
+++ Day_16/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Saurabh Chavan on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
