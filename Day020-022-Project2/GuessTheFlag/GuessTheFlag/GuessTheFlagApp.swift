d3d6220abcf6752ed9fe0891b18540a66109efc1
100rabhcsmc <saurabhchavan052@gmail.com>
2022-12-13 21:49:25 +0530

    Guess the Flag Project2

---
+++ Day020-022-Project2/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Saurabh Chavan on 13/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
