eafd4946f132f32e5a8d79986486d4b644c165a4
100rabhcsmc <saurabhchavan052@gmail.com>
2023-01-19 21:08:38 +0530

    Upgrading our design

--- Day020-022-Project2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Day020-022-Project2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -13,22 +13,39 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     var body: some View {
-        //Showing the player’s score with an alert
+        //Upgrading our design
         ZStack{
-            LinearGradient(gradient: Gradient(colors: [.blue,.black]), startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.2),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26),location:0.2)], center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
-        VStack{
-            Text("Here is Random number \(correctAnswer)").foregroundColor(.white).font(.subheadline.weight(.heavy))
-            Text(countries[correctAnswer]).foregroundColor(.white).font(.largeTitle.weight(.semibold))
-            ForEach(0..<3){number in
-                Button{
-                    //flag is here
-                    flagTapped(number)
-                } label: {
-                    Image(countries[number]).renderingMode(.original).clipShape(Capsule()).shadow(radius: 5)
+            VStack{
+                Spacer()
+                Text("Guess the flag?").font(.largeTitle.bold()).foregroundColor(.white)
+                VStack(spacing: 15){
+                    Text("Here is Random number \(correctAnswer)").foregroundStyle(.secondary).font(.subheadline.weight(.heavy))
+                    Text(countries[correctAnswer]).font(.largeTitle.weight(.semibold))
+                    ForEach(0..<3){number in
+                        Button{
+                            //flag is here
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number]).renderingMode(.original).clipShape(Capsule()).shadow(radius: 5)
+                        }
+                    }
                 }
+                
+                .frame(maxWidth: .infinity)
+                .padding(.vertical,20)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius:  20))
+                Spacer()
+                Spacer()
+                
+                Text("Score ??").font(.title.bold()).foregroundColor(.white)
+                Spacer()
             }
-        }
+            .padding()
     }
         .alert(scoreTitle,isPresented: $showingScore){
             Button("Continue",action: askQuestion)
