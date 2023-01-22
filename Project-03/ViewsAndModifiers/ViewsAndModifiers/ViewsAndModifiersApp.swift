fa6d7f32b4e995716460a577688a1ba4319abfc8
100rabhcsmc <saurabhchavan052@gmail.com>
2023-01-22 22:27:57 +0530

    Project 3 added

---
+++ Project-03/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Saurabh Chavan on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
