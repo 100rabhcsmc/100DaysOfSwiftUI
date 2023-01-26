81afd2dbb184edfc10edb8c9723a60d347b1ef43
100rabhcsmc <saurabhchavan052@gmail.com>
2023-01-26 16:25:20 +0530

    Environment modifiers

--- Project-03/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Project-03/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -11,11 +11,21 @@ struct ContentView: View {
     @State private var colorChange = false;
     var body: some View {
         
-        //Conditional modifiers
-        Button("Please click & change the color"){
-            colorChange.toggle()
-        }.foregroundColor(colorChange ? .blue : .red)
+        //Environment modifiers
+        //Define : Many modifiers can be applied to containers, which allows us to apply the same modifier to many views at the same time This is called an environment modifier.
+        VStack{
+            Text("Sachin").font(.largeTitle)
+            //child take priority
+            Text("Saurabh")
+            Text("Aditya")
+        }.font(.body)
         
+        VStack {
+            Text("Saurabh")
+                .blur(radius: 0)
+            Text("Sachin")
+        }
+        .blur(radius: 10)
     }
 }
 
