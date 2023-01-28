de2d2e3630a863dcfa1bdb7988d47d5e6ea58bb4
100rabhcsmc <saurabhchavan052@gmail.com>
2023-01-29 01:42:41 +0530

    BetterRest: Introduction

---
+++ Project-04/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Saurabh Chavan on 28/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
