8f8a05806a170c07ac2899d1920477d33f2fc784
100rabhcsmc <saurabhchavan052@gmail.com>
2023-01-29 16:40:56 +0530

    Selecting dates and times with DatePicker

--- Project-04/BetterRest/BetterRest/ContentView.swift
+++ Project-04/BetterRest/BetterRest/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    //Entering numbers with Stepper
-    @State private var sleepAmount = 8.0;
+    //Selecting dates and times with DatePicker
+    @State private var wakeUp = Date.now;
     var body: some View {
-        Stepper(" \(sleepAmount.formatted())", value: $sleepAmount,in: 4...12,step: 0.25)
-        //in:4...12 means we can go upto 12 and below 4
-        //step:0.25 means when we click in plus button value increase by 0.25 same for the minus
+        VStack{
+            Text("Hello \(wakeUp.formatted())")
+            DatePicker("Please select the date & time", selection: $wakeUp)
+            
+            DatePicker("Please select the date & time", selection: $wakeUp)
+            //label taking space
+                .labelsHidden()
+            
+            //show me the date or time
+            DatePicker("Please give me the  date or time", selection: $wakeUp,displayedComponents: .date)
+            DatePicker("Please give me the  date or time", selection: $wakeUp,displayedComponents: .hourAndMinute)
+            
+            //we can select only future dates
+            DatePicker("Please select the date & time", selection: $wakeUp,in:Date.now...)
+            
+        }
     }
 }
 
