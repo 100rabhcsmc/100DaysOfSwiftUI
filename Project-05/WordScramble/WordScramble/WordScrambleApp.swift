7df6bd4560c12b708cf991d05c11f6de04050a63
100rabhcsmc <saurabhchavan052@gmail.com>
2023-02-04 15:09:32 +0530

    WordScramble initial

---
+++ Project-05/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Saurabh Chavan on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
