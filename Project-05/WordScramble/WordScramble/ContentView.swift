32ea4ef09ad603b664ad88fd72f5f987e097a120
100rabhcsmc <saurabhchavan052@gmail.com>
2023-02-06 19:51:41 +0530

    Introducing List, your best friend

--- Project-05/WordScramble/WordScramble/ContentView.swift
+++ Project-05/WordScramble/WordScramble/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    var name = ["Sach","Sau","Adi"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            List{
+                Section("Section 1"){
+                    Text("Static row 1")
+                    Text("Static row 2")
+                }
+                Section("Section 2"){
+                    ForEach(1..<3){
+                        Text("\($0)")
+                    }
+
+                }
+                Section("Section 3"){
+                    Text("Static row 3")
+                    Text("Staic row 4")
+                }
+            }.listStyle(.grouped)
+            
+            //Best feature personally like
+            List{
+                Section("Section 2"){
+                    ForEach(1..<3){
+                        Text("\($0)")
+                    }
+                }
+            }.listStyle(.sidebar)
+            
+            //insteat of right forEach we can also write this way
+            List(0..<3){
+               Text("Show the row \($0)")
+            }.listStyle(.sidebar)
+            
+            
+            //forEach on the varibale
+            List(name,id: \.self){
+                Text($0)
+            }
+            
         }
-        .padding()
     }
 }
 
