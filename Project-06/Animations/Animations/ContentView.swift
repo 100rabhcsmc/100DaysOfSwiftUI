3863d252e856f11f5d02f79ff69c03acd1f6e789
100rabhcsmc <saurabhchavan052@gmail.com>
2023-03-09 21:09:39 +0530

    Customizing animations in SwiftUI

--- Project-06/Animations/Animations/ContentView.swift
+++ Project-06/Animations/Animations/ContentView.swift
@@ -10,18 +10,45 @@ import SwiftUI
 struct ContentView: View {
     //Creating implicit animations
     @State private var animationAmount = 1.0
+    @State private var animationAmount2 = 1.0
     
     var body: some View {
+        VStack{
+            Button("Tap me"){
+                animationAmount += 0.3
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+            )
+            .scaleEffect(animationAmount)
+            .animation(.easeInOut(duration: 3).repeatForever(autoreverses: true), value: animationAmount)
+        }
+        
+        //second
         Button("Tap me"){
-            animationAmount += 0.3
+            //animationAmount += 0.3
         }
         .padding(50)
         .background(.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .animation(.default, value: animationAmount)
-    }
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount2)
+                .opacity(2 - animationAmount2)
+                .animation(.easeInOut(duration: 3).repeatForever(autoreverses: true), value: animationAmount2)
+        ).onAppear {
+            animationAmount2 = 2
+        }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
