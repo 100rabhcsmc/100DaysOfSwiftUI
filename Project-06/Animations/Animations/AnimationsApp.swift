6cd84ae8ee61daea7fef4f64ae26da04ebe61d16
100rabhcsmc <saurabhchavan052@gmail.com>
2023-03-09 19:49:05 +0530

    Project-06-animation

---
+++ Project-06/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Saurabh Chavan on 09/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
