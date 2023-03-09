8020a81b20d5bf50643f409cb42148688a13ecfa
100rabhcsmc <saurabhchavan052@gmail.com>
2023-03-10 00:02:36 +0530

    Animating bindings

--- Project-06/Animations/Animations/ContentView.swift
+++ Project-06/Animations/Animations/ContentView.swift
@@ -8,48 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    //Creating implicit animations
+    //Animating bindings
     @State private var animationAmount = 1.0
-    @State private var animationAmount2 = 1.0
-    
+  
     var body: some View {
-        VStack(spacing: 100){
+        print(animationAmount)
+         return VStack{
+             Stepper("Scale Amount", value: $animationAmount.animation(.easeInOut(duration: 1).repeatCount(3,autoreverses: true)),in: 1...10)
+            Spacer()
             Button("Tap me"){
-                animationAmount += 0.3
+                animationAmount += 1
             }
             .padding(50)
             .background(.red)
             .foregroundColor(.white)
             .clipShape(Circle())
-            .overlay(
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-            )
             .scaleEffect(animationAmount)
-            .animation(.easeInOut(duration: 3).repeatForever(autoreverses: true), value: animationAmount)
-            
-            
-            //second
-            Button("Tap me"){
-                //animationAmount += 0.3
-            }
-            .padding(50)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .overlay(
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmount2)
-                    .opacity(2 - animationAmount2)
-                    .animation(.easeInOut(duration: 3).repeatForever(autoreverses: true), value: animationAmount2)
-            ).onAppear {
-                animationAmount2 = 2
-            }
         }
-        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
