6136a35bd414e32c6eb4b0a799380f46b5c86da6
100rabhcsmc <saurabhchavan052@gmail.com>
2023-03-12 20:47:42 +0530

    custom transitions using ViewModifier

--- Project-06/Animations/Animations/ContentView.swift
+++ Project-06/Animations/Animations/ContentView.swift
@@ -5,26 +5,47 @@
 //  Created by Saurabh Chavan on 09/03/23.
 //
 
+////Building custom transitions using ViewModifier
+
 import SwiftUI
 
+struct cornerRorateModifier : ViewModifier{
+    let amount : Double
+    let anchor : UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+        .rotationEffect(.degrees(amount),anchor: anchor)
+        .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot :  AnyTransition{
+        .modifier(active: cornerRorateModifier(amount: -90, anchor: .topLeading), identity: cornerRorateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
-    //Animating gestures
     @State private var isShowingRed = false
     
   
     var body: some View {
-        VStack{
-            Button("Tape me"){
-                withAnimation{
-                    isShowingRed.toggle()
-                }
-            }
-            if isShowingRed{
+        ZStack{
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200,height: 200)
+            
+            if isShowingRed {
                 Rectangle()
                     .fill(.red)
                     .frame(width: 200,height: 200)
-                    //.transition(.scale)
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation{
+                isShowingRed.toggle()
             }
         }
     }
