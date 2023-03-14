1cbd731dd0180890c46a4e7aefe0cbce259499ff
100rabhcsmc <saurabhchavan052@gmail.com>
2023-03-14 23:36:16 +0530

    iExpense

---
+++ Project-07/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Saurabh Chavan on 14/03/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
