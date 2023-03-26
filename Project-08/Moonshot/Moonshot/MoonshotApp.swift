7424c3c7dc991fbceafbcbb8ef948470d444aa24
100rabhcsmc <saurabhchavan052@gmail.com>
2023-03-27 00:47:47 +0530

    Moonshot: Introduction

---
+++ Project-08/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Saurabh Chavan on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
